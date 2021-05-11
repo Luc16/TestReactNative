2391e6bee09f1178d5942dc56c660f00f9eb7f22
Luc16 <33912482+Luc16@users.noreply.github.com>
2021-05-10 22:30:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -12,12 +12,6 @@ export default class testapp extends Component{
   constructor(props){
     super(props)
     const firebaseConfig = {
-      authDomain: "testapp-16d9b.firebaseapp.com",
-      databaseURL: "https://testapp-16d9b.firebaseio.com",
-      projectId: "testapp-16d9b",
-      storageBucket: "testapp-16d9b.appspot.com",
-      messagingSenderId: "1086886942192",
-      appId: "1:1086886942192:web:8c1a9cc992d29ba7"
     }
     firebase.initializeApp(firebaseConfig)
   }
